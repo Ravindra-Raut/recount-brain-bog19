b3c1271254bf02a4de01473c5bd9898c4fb937f9
Leonardo Collado Torres <lcollado@jhsph.edu>
2019-05-06 14:15:56 -0400

    Add R code for reproducibility and image I tweeted at https://twitter.com/fellgernon/status/1125463243538010118

---
+++ get_reproducibility_info.R
@@ -0,0 +1,11 @@
+## Reproducibility information:
+library('posterdown')
+library('sessioninfo')
+options(width = 120)
+session_info()
+
+## Citation information for recount-brain
+citation('recount')[5]
+
+## For posterdown:
+citation('posterdown')
